5d5829496b877181a3bc473abc835b88e5c819e4
Jakub Sarnecki <Jakub.Sarnecki@vu.com>
2022-01-18 15:27:19 -0600

    1527

--- payCalculator.py
+++ payCalculator.py
@@ -3,13 +3,13 @@ def calculatePay():
     print("calculating pay")
     # This first line is provided for you
     hrs = float(input("Enter Hours: "))
-    hpay = float(input("Hourly Wage?: "))884
+    hpay = float(input("Hourly Wage?: "))
     gpay =hrs*hpay
-    print("Your Gross Pay is: ",gpay)
+    print (str(gpay))
     # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-#calculatePay()
+# calculatePay()
